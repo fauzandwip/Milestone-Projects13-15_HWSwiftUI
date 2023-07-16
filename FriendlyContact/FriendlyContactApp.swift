f6d41d1426960139255bb6c95f6026e3d1ebdf92
fauzandp <fauzan27dp@gmail.com>
2023-07-16 10:51:47 +0700

    Initial Commit

---
+++ FriendlyContact/FriendlyContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendlyContactApp.swift
+//  FriendlyContact
+//
+//  Created by Fauzan Dwi Prasetyo on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendlyContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
